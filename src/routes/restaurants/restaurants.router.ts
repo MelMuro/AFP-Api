17fc1ac4546bb7ca741ea49cbe048c2ed7b9f11f
Mel <el_manuel_muro@hotmail.com>
2024-04-28 10:08:03 -0700

    Add linter and formatter to project

--- src/routes/restaurants/restaurants.router.ts
+++ src/routes/restaurants/restaurants.router.ts
@@ -1,11 +1,13 @@
 import { Request, Response, Router } from 'express';
-import { dbCollections } from '../../db/client'
+import { dbCollections } from '../../db/client';
 import Restaurant from './restaurant.model';
 
 export const restaurantsRouter = Router();
 
 restaurantsRouter.get('/', async (req: Request, res: Response) => {
-    const restaurants = await dbCollections.Restaurants?.find<Restaurant>({}).toArray();
+	const restaurants = await dbCollections.Restaurants?.find<Restaurant>(
+		{}
+	).toArray();
 
-    res.status(200).send(restaurants);
+	res.status(200).send(restaurants);
 });
