2926e7f1c873e690b0aa3d530c18b06e3ed2c52e
Mel <el_manuel_muro@hotmail.com>
2024-04-29 19:52:56 -0700

    Clean up code

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,18 @@
-import express, { Request, Response , Application } from 'express';
-import { restaurantsRouter }  from './routes/restaurants/restaurants.router'
-import { connectToDb } from './db/client'
-import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import express, { Request, Response, Application } from "express";
+import { restaurantsRouter } from "./routes/restaurants/restaurants.router";
+import { connectToDb } from "./db/client";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Application = express();
 
-if (process.env.NODE_ENV === 'development') {
-    connectToDb();
+if (process.env.NODE_ENV === "development") {
+  connectToDb();
 }
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }))
-app.get('/', (req: Request, res: Response) => {
-    res.send({result: 'Welcome to Express & TypeScript Server'});
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/restaurants", restaurantsRouter);
 
--- src/routes/restaurants/restaurants.router.test.ts
+++ src/routes/restaurants/restaurants.router.test.ts
@@ -1,39 +1,34 @@
-import supertest from 'supertest';
-import app from '../../app'
-import * as db from '../../db/client.mock';
-import Restaurant from './restaurant.model';
+import supertest from "supertest";
+import app from "../../app";
+import * as db from "../../db/client.mock";
+import Restaurant from "./restaurant.model";
 
 const request = supertest(app);
 
-describe('Restaurants Router tests', () => {
-    beforeAll(async () => {
-      await db.connect();
-    });
+describe("Restaurants Router tests", () => {
+  beforeAll(async () => {
+    await db.connect();
+  });
 
-    beforeEach(async () => {
-      await db.seedDatabase();
-    });
+  beforeEach(async () => {
+    await db.seedDatabase();
+  });
 
-    afterEach(async () => {
-      await db.clearDatabase()
-    });
+  afterEach(async () => {
+    await db.clearDatabase();
+  });
 
-    afterAll(async () => {
-      await db.close();
-    });
+  afterAll(async () => {
+    await db.close();
+  });
 
-    it.skip('should load default endpoint', async () => {
-      const res = await request.get('/');
-      
-      expect(res.statusCode).toBe(200);
-      expect(res.body.result).toBe('Welcome to Express & TypeScript Server');
-    });
+  it("should get all restaurants", async () => {
+    //Act
+    const res = await request.get("/restaurants");
+    const restaurants = res.body as Restaurant[];
 
-    it('should get all restaurants', async () => {
-      const res = await request.get('/restaurants');
-      const restaurants = (res.body as Restaurant[]);
-
-      expect(res.statusCode).toBe(200);
-      expect(restaurants.pop()?.name).toBe('Test Restaurant 1');
-    });
+    //Assert
+    expect(res.statusCode).toBe(200);
+    expect(restaurants[0].name).toBe("Test Restaurant 1");
+  });
 });
