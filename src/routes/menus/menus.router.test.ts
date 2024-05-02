acf50ee884f28f388ac99cd3feb0b6a9978c6360
Jesús Iván Romero Valenzuela <iromero@necodex.com>
2024-05-02 12:54:02 -0700

    Test post

--- src/routes/menus/menus.router.test.ts
+++ src/routes/menus/menus.router.test.ts
@@ -14,9 +14,9 @@ describe('Menus Router tests', () => {
         await db.seedDatabase();
     });
 
-    // afterEach(async () => {
-    //     await db.clearDatabase();
-    // })
+    afterEach(async () => {
+        await db.clearDatabase();
+    })
 
     afterAll(async () => {
         await db.close();
@@ -43,13 +43,41 @@ describe('Menus Router tests', () => {
 
     it('should get a dish from by restaurant', async () => {
         //Act
-        const res = await request.get('/menus/Test%20Restaurant%201/chilaquiles');
+        const res = await request.get('/menus/Test Restaurant 1/chilaquiles');
         const menus = res.body as Menus;
 
         //Assert
         expect(res.statusCode).toBe(200);
         expect(menus.restaurant).toBe("Test Restaurant 1");
         expect(menus.dishes[0].name).toBe("chilaquiles");
+    });
+
+    it('should post a menu', async () => {
+        //Act
+        const mockMenu = {
+            restaurant: 'Sushito',
+            dishes: [
+                {
+                    category: 'Entrada,',
+                    name: 'Taco yaqui',
+                    description: 'totopos de maiz con salsa y queso',
+                    price: 120,
+                    picture: 'test_picture50',
+                    isAvailable: true,
+                    tag: 'empanizado'
+                }
+            ]
+        };
+        const res = await request.post('/menus')
+            .send(mockMenu)
+
+        const getNewRestaurant = await request.get(`/menus/${mockMenu.restaurant}/${mockMenu.dishes[0].name}`);
+        const menus = getNewRestaurant.body as Menus;
+
+        //Assert
+        expect(res.statusCode).toBe(200);
+        expect(menus.restaurant).toBe(mockMenu.restaurant);
+        expect(menus.dishes[0].name).toBe(mockMenu.dishes[0].name);
     });
 
 });
