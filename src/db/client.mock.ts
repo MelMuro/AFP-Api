8e996e8830ab35e4f55ba7d1f1fbea54ae7fe668
MelMuro <el_manuel_muro@hotmail.com>
2024-05-06 09:39:19 -0700

    Fix tests setup and teardown (#7)

--- src/db/client.mock.ts
+++ src/db/client.mock.ts
@@ -1,63 +1,65 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import { MongoClient } from 'mongodb'
-import { dbCollections } from '../db/client'
+import { MongoClient } from 'mongodb';
+import { dbCollections } from '../db/client';
 
-let mongoServer: MongoMemoryServer
+let mongoServer: MongoMemoryServer;
 let mongoClient: MongoClient;
 
 export const connect = async () => {
-    mongoServer = await MongoMemoryServer.create();
-    mongoClient = await MongoClient.connect(mongoServer.getUri(), {});
+	mongoServer = await MongoMemoryServer.create();
+	mongoClient = await MongoClient.connect(mongoServer.getUri(), {});
 };
 
 export const close = async () => {
-    await mongoClient.db().dropDatabase();
-    await mongoClient.close();
-    await mongoServer.stop();
+	await mongoClient.db().dropDatabase();
+	await mongoClient.close();
+	await mongoServer.stop();
 };
 
-export const clearDatabase = async () => {
-    (await mongoClient.db().collections()).forEach((col) => col.deleteMany({}));
+export const clearDatabase = () => {
+	mongoClient.db().dropDatabase();
 };
 
-export const seedDatabase = async () => {
-    dbCollections.Restaurants = mongoClient.db().collection(process.env.DB_RESTAURANTS_COLLECTION);
-    dbCollections.Menus = mongoClient.db().collection(process.env.DB_MENUS_COLLECTION);
+export const seedDatabase = () => {
+	dbCollections.Restaurants = mongoClient
+		.db()
+		.collection(process.env.DB_RESTAURANTS_COLLECTION);
+	dbCollections.Menus = mongoClient
+		.db()
+		.collection(process.env.DB_MENUS_COLLECTION);
 
-    dbCollections.Restaurants.insertOne({
-        "name": "Test Restaurant 1",
-        "description": "Some test description",
-        "category": "Test Category",
-        "location": 1,
-        "schedule": {
-            "monday": "8:00 - 17:00",
-            "tuesday": "8:00 - 17:00",
-            "wednesday": "8:00 - 17:00",
-            "thursday": "8:00 - 17:00",
-            "friday": "8:00 - 17:00",
-            "saturday": "8:00 - 17:00",
-            "sunday": "8:00 - 17:00"
-        },
-        "phone": 6621234567,
-        "email": "test@mail.com",
-        "media": ["testLink.com", "testLink2.com"],
-        "pictures": [
-            "test_pic1"
-        ]
-    });
+	dbCollections.Restaurants.insertOne({
+		name: 'Test Restaurant 1',
+		description: 'Some test description',
+		category: 'Test Category',
+		location: 1,
+		schedule: {
+			monday: '8:00 - 17:00',
+			tuesday: '8:00 - 17:00',
+			wednesday: '8:00 - 17:00',
+			thursday: '8:00 - 17:00',
+			friday: '8:00 - 17:00',
+			saturday: '8:00 - 17:00',
+			sunday: '8:00 - 17:00'
+		},
+		phone: 6621234567,
+		email: 'test@mail.com',
+		media: ['testLink.com', 'testLink2.com'],
+		pictures: ['test_pic1']
+	});
 
-    dbCollections.Menus.insertOne({
-        "restaurant": "Test Restaurant 1",
-        "dishes": [
-            {
-                "category": "plato fuerte",
-                "name": "chilaquiles",
-                "description": "totopos de maiz con salsa y queso",
-                "price": 120,
-                "picture": "test_picture1",
-                "isAvailable": true,
-                "tag": 'enchiloso'
-            }
-        ]
-    });
+	dbCollections.Menus.insertOne({
+		restaurant: 'Test Restaurant 1',
+		dishes: [
+			{
+				category: 'plato fuerte',
+				name: 'chilaquiles',
+				description: 'totopos de maiz con salsa y queso',
+				price: 120,
+				picture: 'test_picture1',
+				isAvailable: true,
+				tag: 'enchiloso'
+			}
+		]
+	});
 };
--- src/routes/restaurants/restaurants.router.test.ts
+++ src/routes/restaurants/restaurants.router.test.ts
@@ -6,20 +6,20 @@ import Restaurant from './restaurant.model';
 const request = supertest(app);
 
 describe('Restaurants Router tests', () => {
-	beforeAll(async () => {
-		await db.connect();
+	beforeAll(() => {
+		return db.connect();
 	});
 
-	beforeEach(async () => {
-		await db.seedDatabase();
+	beforeEach(() => {
+		db.seedDatabase();
 	});
 
-	afterEach(async () => {
-		await db.clearDatabase();
+	afterEach(() => {
+		db.clearDatabase();
 	});
 
-	afterAll(async () => {
-		await db.close();
+	afterAll(() => {
+		return db.close();
 	});
 
 	it('should get all restaurants', async () => {
@@ -32,41 +32,49 @@ describe('Restaurants Router tests', () => {
 		expect(restaurants[0].name).toBe('Test Restaurant 1');
 	});
 
-	//POST NEW RESTAURANT
+	it('should get only one restaurant', async () => {
+		//Act
+		const res = await request.get('/restaurants/Test%20Restaurant%201');
+		const restaurant = res.body as Restaurant;
+
+		//Assert
+		expect(res.statusCode).toBe(200);
+		expect(restaurant.location).toBe(1);
+	});
+
 	it('should create a new restaurant', async () => {
 		// Arrange
 		const newRestaurant = {
-			"name": "Akitabara",
-			"description": "Casa ramen",
-			"category": "Comida japonesa",
-			"location": 3,
-			"schedule": {
-				"monday": "Cerrado",
-				"tuesday": "12:00 pm a 8:00 pm",
-				"wednesday": "12:00 pm a 8:00 pm",
-				"thursday": "12:00 pm a 8:00 pm",
-				"friday": "12:00 pm a 8:00 pm",
-				"saturday": "12:00 pm a 8:00 pm",
-				"sunday": "2:00 pm a 6:00 pm"
+			name: 'Akitabara',
+			description: 'Casa ramen',
+			category: 'Comida japonesa',
+			location: 3,
+			schedule: {
+				monday: 'Cerrado',
+				tuesday: '12:00 pm a 8:00 pm',
+				wednesday: '12:00 pm a 8:00 pm',
+				thursday: '12:00 pm a 8:00 pm',
+				friday: '12:00 pm a 8:00 pm',
+				saturday: '12:00 pm a 8:00 pm',
+				sunday: '2:00 pm a 6:00 pm'
 			},
-			"phone": 6624670000,
-			"email": "akita@bara.com",
-			"media": ["https://www.instagram.com/akitabara/", "https://www.facebook.com/AkitabaraCasaRamen"],
-			"pictures": ["https://scontent.cdninstagram.com/v/t39.30808-6/406328835_18328903135100950_2743190508086425728_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMzQ5eDE2ODcuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=110&_nc_ohc=eQPPHJ8FEkwAb6zxA6s&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzI0Njk5MTA5OTIxODY5OTYzOA%3D%3D.2-ccb7-5&oh=00_AfDIHbV6trOvBSnrPCl68jWAmLFHDJdpoBWVvMQ_mE_kkQ&oe=662D48B0&_nc_sid=10d13b", "https://scontent.cdninstagram.com/v/t39.30808-6/341262521_903843497338952_7787466491387203718_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=4_D0QKtQoZQAb6uFIwJ&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzA5MjYxNDU1NDYxNDk1NjcyNQ%3D%3D.2-ccb7-5&oh=00_AfDvPKJL-A55Sw6biAEwzL3c3YC_FXByAC-5vl_edRKYiw&oe=662D1D02&_nc_sid=10d13b"]
-		
-		}
-	
+			phone: 6624670000,
+			email: 'akita@bara.com',
+			media: [
+				'https://www.instagram.com/akitabara/',
+				'https://www.facebook.com/AkitabaraCasaRamen'
+			],
+			pictures: [
+				'https://scontent.cdninstagram.com/v/t39.30808-6/406328835_18328903135100950_2743190508086425728_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMzQ5eDE2ODcuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=110&_nc_ohc=eQPPHJ8FEkwAb6zxA6s&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzI0Njk5MTA5OTIxODY5OTYzOA%3D%3D.2-ccb7-5&oh=00_AfDIHbV6trOvBSnrPCl68jWAmLFHDJdpoBWVvMQ_mE_kkQ&oe=662D48B0&_nc_sid=10d13b',
+				'https://scontent.cdninstagram.com/v/t39.30808-6/341262521_903843497338952_7787466491387203718_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=108&_nc_ohc=4_D0QKtQoZQAb6uFIwJ&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzA5MjYxNDU1NDYxNDk1NjcyNQ%3D%3D.2-ccb7-5&oh=00_AfDvPKJL-A55Sw6biAEwzL3c3YC_FXByAC-5vl_edRKYiw&oe=662D1D02&_nc_sid=10d13b'
+			]
+		};
+
 		// Act
 		const res = await request.post('/restaurants').send(newRestaurant);
-	
+
 		// Assert
 		expect(res.statusCode).toBe(201);
 		expect(res.text).toBe('Successfully created a new restaurant');
 	});
-
-
-
-
-	  
 });
-
