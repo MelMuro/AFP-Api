2c4e5ae738c4627b6c82d92ac9b16402f7e5c074
MelMuro <el_manuel_muro@hotmail.com>
2024-07-08 19:19:28 -0700

    Feature/update schedule (#12)
    * update schedule type on restauratns
    
    * Update test setup
    
    * Update type name

--- src/routes/restaurants/restaurant.model.ts
+++ src/routes/restaurants/restaurant.model.ts
@@ -1,19 +1,17 @@
 import { ObjectId } from 'mongodb';
 
+type Day = {
+	name: string;
+	start: string;
+	end: string;
+};
+
 type Restaurant = {
 	name: string;
 	description: string;
 	category: string;
 	location: number;
-	schedule: {
-		monday: string;
-		tuesday: string;
-		wednesday: string;
-		thursday: string;
-		friday: string;
-		saturday: string;
-		sunday: string;
-	};
+	schedule: Day[];
 	phone: number;
 	email: string;
 	media: {
